eb40abaa7cff3c26907b5fa2c08e9acead505c00
Randy <randykeller1193@gmail.com>
2021-11-27 16:00:14 -0500

    moved video screen to left side

--- src/components/WebCamScreen.js
+++ src/components/WebCamScreen.js
@@ -4,11 +4,11 @@ import Video from "./Video";
 function WebCamScreen() {
   return (
     <>
-      <Video scale={[1, 0.7, 1]} position={[-1, 2, 0]} />
+      <Video scale={[1, 0.7, 1]} position={[1, 2, 0]} />
       <Video
         rotation={[0, Math.PI / 1, 0]}
         scale={[1, 0.7, 1]}
-        position={[-1, 2, 0]}
+        position={[1, 2, 0]}
       />
     </>
   );
